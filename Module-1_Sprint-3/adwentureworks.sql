7d46a004e0a3121a14796dd9bf4cd4bb1e814120
Giedrius77 <giedrius.zalionis@gmail.com>
2024-05-03 13:51:43 +0300

    adwentureworks 1.3 - done

--- Module-1_Sprint-3/adwentureworks.sql
+++ Module-1_Sprint-3/adwentureworks.sql
@@ -10,7 +10,7 @@ FROM `tc-da-1.adwentureworks_db.product` AS p
 JOIN `adwentureworks_db.productsubcategory` AS psc
 ON p.ProductSubcategoryID = psc.ProductSubcategoryID
 ORDER BY SubCategory
-LIMIT 6;
+LIMIT 5;
 
 -- 1.2 In 1.1 query you have a product subcategory but see that you could use the category name.
 
@@ -25,3 +25,35 @@ JOIN `adwentureworks_db.productcategory` AS pc
 ON psc.ProductCategoryID = pc.ProductCategoryID
 ORDER BY pc.Name
 LIMIT 5;
+
+-- 1.3 Use the established query to select the most expensive (price listed over 2000) bikes that are still actively sold (does not have a sales end date)
+
+-- Order the results from most to least expensive bike.
+
+SELECT p.ProductID, p.Name, p.ProductNumber, p.ListPrice, psc.Name AS SubCategory, pc.Name AS Category
+FROM `tc-da-1.adwentureworks_db.product` AS p
+JOIN `adwentureworks_db.productsubcategory` AS psc
+ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+JOIN `adwentureworks_db.productcategory` AS pc
+ON psc.ProductCategoryID = pc.ProductCategoryID
+WHERE (pc.Name = 'Bikes' AND p.ListPrice > 2000) AND p.SellEndDate IS NULL
+ORDER BY p.ListPrice DESC
+LIMIT 5;
+
+-- Rows counts per individual tables
+
+SELECT 
+    (SELECT COUNT(*) FROM `tc-da-1.adwentureworks_db.product`) AS Total_ProductID,
+    (SELECT COUNT(*) FROM `adwentureworks_db.productsubcategory`) AS Total_ProductSubcategoryID,
+    (SELECT COUNT(*) FROM `adwentureworks_db.productcategory`) AS Total_ProductCategoryID
+    
+
+-- 2. Reviewing work orders
+
+-- 2.1 Create an aggregated query to select the:
+
+-- Number of unique work orders.
+-- Number of unique products.
+-- Total actual cost.
+-- For each location Id from the 'workoderrouting' table for orders in January 2004.
+
