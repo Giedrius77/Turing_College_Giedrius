3064aea68c19075a0f220bb98e27064e471c26b8
Giedrius77 <giedrius.zalionis@gmail.com>
2024-05-06 12:00:37 +0300

    adwentureworks 3.1 and 3.2 done

--- Module-1_Sprint-3/adwentureworks.sql
+++ Module-1_Sprint-3/adwentureworks.sql
@@ -115,3 +115,56 @@ LIMIT 5;
 
 -- 3.1 Your colleague has written a query to find the list of orders connected to special offers. The query works fine but the numbers are off, investigate where the potential issue lies.
 
+-- First JOIN path was inccorect, instead of productID must be SpecialOfferID
+
+SELECT sales_detail.SalesOrderId
+,sales_detail.OrderQty
+,sales_detail.UnitPrice
+,sales_detail.LineTotal
+,sales_detail.ProductId
+,sales_detail.SpecialOfferID
+,spec_offer_product.ModifiedDate
+,spec_offer.Category
+,spec_offer.Description
+
+FROM `tc-da-1.adwentureworks_db.salesorderdetail` AS sales_detail
+
+LEFT JOIN `tc-da-1.adwentureworks_db.specialofferproduct` AS spec_offer_product
+ON sales_detail.SpecialOfferID = spec_offer_product.SpecialOfferID
+
+LEFT JOIN `tc-da-1.adwentureworks_db.specialoffer` AS spec_offer
+ON sales_detail.SpecialOfferID = spec_offer.SpecialOfferID
+
+ORDER BY LineTotal DESC
+LIMIT 20;
+
+
+-- 3.2 Your colleague has written this query to collect basic Vendor information. The query does not work, look into the query and find ways to fix it. Can you provide any feedback on how to make this query be easier to debug/read?
+
+-- Fixed and debugged: make all allies easy to read, complete all joins based on SELECT query.
+
+SELECT vendor.VendorID AS NameID
+,vendor.Name
+,vendor.CreditRating
+,vendor.ActiveFlag
+,vendor_contact.ContactId
+,contacttype.ContactTypeId
+,vendoraddress.AddressId
+,address.City
+
+FROM `tc-da-1.adwentureworks_db.vendor` AS vendor
+
+JOIN `tc-da-1.adwentureworks_db.vendorcontact` AS vendor_contact 
+ON vendor.VendorID = vendor_contact.VendorID
+
+LEFT JOIN `tc-da-1.adwentureworks_db.contacttype` AS contacttype
+ON vendor_contact.ContactTypeID = contacttype.ContactTypeID
+
+LEFT JOIN `tc-da-1.adwentureworks_db.vendoraddress` AS vendoraddress
+ON vendor.VendorID = vendoraddress.VendorID
+
+LEFT JOIN `tc-da-1.adwentureworks_db.address` AS address 
+ON vendoraddress.AddressID = address.AddressID
+
+ORDER BY NameID
+LIMIT 5;
