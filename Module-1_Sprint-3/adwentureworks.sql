5daa9132f451be2700653814c4a531be0526cb8c
Giedrius77 <giedrius.zalionis@gmail.com>
2024-05-07 09:46:12 +0300

    modiffied all tasks

--- Module-1_Sprint-3/adwentureworks.sql
+++ Module-1_Sprint-3/adwentureworks.sql
@@ -13,8 +13,10 @@ SELECT p.ProductID
       ,psc.ProductSubcategoryID
       ,psc.Name AS SubCategory
 FROM `tc-da-1.adwentureworks_db.product` AS p
+
 JOIN `tc-da-1.adwentureworks_db.productsubcategory` AS psc
 ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+
 ORDER BY SubCategory;
 
 -- 1.2 In 1.1 query you have a product subcategory but see that you could use the category name.
@@ -31,10 +33,13 @@ SELECT p.ProductID
       ,psc.Name AS SubCategoryName
       ,pc.Name AS Category
 FROM `tc-da-1.adwentureworks_db.product` AS p
+
 JOIN `tc-da-1.adwentureworks_db.productsubcategory` AS psc
 ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+
 JOIN `tc-da-1.adwentureworks_db.productcategory` AS pc
 ON psc.ProductCategoryID = pc.ProductCategoryID
+
 ORDER BY pc.Name;
 
 -- 1.3 Use the established query to select the most expensive (price listed over 2000) bikes that are still actively sold (does not have a sales end date)
@@ -48,10 +53,13 @@ SELECT p.ProductID
       ,psc.Name AS SubCategory
       ,pc.Name AS Category
 FROM `tc-da-1.adwentureworks_db.product` AS p
+
 JOIN `tc-da-1.adwentureworks_db.productsubcategory` AS psc
 ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+
 JOIN `tc-da-1.adwentureworks_db.productcategory` AS pc
 ON psc.ProductCategoryID = pc.ProductCategoryID
+
 WHERE (pc.Name = 'Bikes' AND p.ListPrice > 2000) AND p.SellEndDate IS NULL
 ORDER BY p.ListPrice DESC;
 
@@ -73,16 +81,21 @@ SELECT
 -- For each location Id from the 'workoderrouting' table for orders in January 2004.
 
 SELECT l.LocationID
-      ,COUNT(wo.OrderQty) AS no_work_orders
+      ,COUNT(wo.WorkOrderID) AS no_work_orders
       ,COUNT(DISTINCT p.Name) AS no_unique_product
       ,SUM(wor.ActualCost) AS actual_cost
 FROM `tc-da-1.adwentureworks_db.workorderrouting` AS wor
+
 JOIN `tc-da-1.adwentureworks_db.location` AS l
 ON wor.LocationID = l.LocationID
+
 JOIN `tc-da-1.adwentureworks_db.workorder` AS wo
 ON wor.WorkOrderID = wo.WorkOrderID
+
 JOIN `tc-da-1.adwentureworks_db.product` AS p
 ON wo.ProductID = p.ProductID
+
+-- WHERE wor.ModifiedDate BETWEEN '2004-01-01' AND '2004-01-31'
 WHERE wor.ModifiedDate BETWEEN '2004-01-01' AND '2004-02-01'
 GROUP BY l.LocationID
 ORDER BY SUM(wor.ActualCost) DESC;
@@ -94,35 +107,33 @@ SELECT l.LocationID, l.Name,
   COUNT(wo.OrderQty) AS no_work_orders, 
   COUNT(DISTINCT p.Name) AS no_unique_product, 
   SUM(wor.ActualCost) AS actual_cost, 
-  CAST(AVG(DATE_DIFF(wor.ActualEndDate, wor.ActualStartDate, day))AS DECIMAL) AS avg_days_diff
+  ROUND(AVG(DATE_DIFF(wor.ActualEndDate, wor.ActualStartDate, day)),2) AS avg_days_diff
 FROM `tc-da-1.adwentureworks_db.workorderrouting` AS wor
-  JOIN `tc-da-1.adwentureworks_db.product` AS p
-  ON wor.ProductID = p.ProductID
-  JOIN `tc-da-1.adwentureworks_db.location` AS l
-  ON wor.LocationID = l.LocationID
-  JOIN `tc-da-1.adwentureworks_db.workorder` AS wo
-  ON wor.WorkOrderID = wo.WorkOrderID
-WHERE wor.ModifiedDate BETWEEN '2004-01-01' AND '2004-02-01'
+  
+JOIN `tc-da-1.adwentureworks_db.product` AS p
+ON wor.ProductID = p.ProductID
+
+JOIN `tc-da-1.adwentureworks_db.location` AS l
+ON wor.LocationID = l.LocationID
+
+JOIN `tc-da-1.adwentureworks_db.workorder` AS wo
+ON wor.WorkOrderID = wo.WorkOrderID
+
+WHERE wor.ModifiedDate BETWEEN '2004-01-01' AND '2004-01-31'
 GROUP BY l.LocationID, l.Name
 ORDER BY SUM(wor.ActualCost) DESC;
 
 -- 2.3 Select all the expensive work Orders (above 300 actual cost) that happened throught January 2004.
--- I can't find right solution regard ActualCost above 300!!!! 
 
-SELECT wo.WorkOrderID AS WorkOrderID, wor.ActualCost AS actual_cost
+SELECT wo.WorkOrderID AS WorkOrderID, SUM(wor.ActualCost) AS actual_cost
 FROM `tc-da-1.adwentureworks_db.workorderrouting` AS wor
+
 JOIN `tc-da-1.adwentureworks_db.workorder` AS wo
 ON wor.WorkOrderID = wo.WorkOrderID 
--- JOIN `adwentureworks_db.product`AS p
--- ON wo.ProductID = p.ProductID
--- JOIN `adwentureworks_db.transactionhistory` AS th
--- ON th.ProductID = p.ProductID
--- JOIN `adwentureworks_db.transactionhistoryarchive` AS tha
--- ON th.TransactionID = tha.TransactionID
-WHERE wor.ModifiedDate BETWEEN '2004-01-01' AND '2004-02-01'
-ORDER BY actual_cost
-LIMIT 5;
--- WHERE wor.ActualCost > 300 AND
+
+WHERE wor.ModifiedDate BETWEEN '2004-01-01' AND '2004-01-31'
+GROUP BY wo.WorkOrderID
+HAVING SUM(wor.ActualCost) > 300
 
 
 -- Task 3. Query validation
@@ -132,8 +143,6 @@ LIMIT 5;
 -- Once you have it fixed, copy into your spreadsheet of results among previous task results.
 
 -- 3.1 Your colleague has written a query to find the list of orders connected to special offers. The query works fine but the numbers are off, investigate where the potential issue lies.
-
--- First JOIN path was inccorect, instead of productID must be SpecialOfferID
 
 SELECT sales_detail.SalesOrderId
 ,sales_detail.OrderQty
@@ -148,7 +157,7 @@ SELECT sales_detail.SalesOrderId
 FROM `tc-da-1.adwentureworks_db.salesorderdetail` AS sales_detail
 
 LEFT JOIN `tc-da-1.adwentureworks_db.specialofferproduct` AS spec_offer_product
-ON sales_detail.SpecialOfferID = spec_offer_product.SpecialOfferID
+ON sales_detail.SpecialOfferID = spec_offer_product.SpecialOfferID AND sales_detail.ProductID = spec_offer_product.ProductID
 
 LEFT JOIN `tc-da-1.adwentureworks_db.specialoffer` AS spec_offer
 ON sales_detail.SpecialOfferID = spec_offer.SpecialOfferID
@@ -158,26 +167,21 @@ ORDER BY LineTotal DESC;
 
 -- 3.2 Your colleague has written this query to collect basic Vendor information. The query does not work, look into the query and find ways to fix it. Can you provide any feedback on how to make this query be easier to debug/read?
 
--- Fixed and debugged: make all allies easy to read, complete all joins based on SELECT query.
-
 SELECT vendor.VendorID AS NameID
       ,vendor.Name
       ,vendor.CreditRating
       ,vendor.ActiveFlag
       ,vendorcontact.ContactId
-      ,contacttype.ContactTypeId
+      ,vendorcontact.ContactTypeId
       ,vendoraddress.AddressId
       ,address.City
 
 FROM `tc-da-1.adwentureworks_db.vendor` AS vendor
 
-JOIN `tc-da-1.adwentureworks_db.vendorcontact` AS vendorcontact 
+LEFT JOIN `tc-da-1.adwentureworks_db.vendorcontact` AS vendorcontact 
 ON vendor.VendorID = vendorcontact.VendorID
 
-LEFT JOIN `tc-da-1.adwentureworks_db.contacttype` AS contacttype
-ON vendorcontact.ContactTypeID = contacttype.ContactTypeID
-
-LEFT JOIN `tc-da-1.adwentureworks_db.vendoraddress` AS vendoraddress
+JOIN `tc-da-1.adwentureworks_db.vendoraddress` AS vendoraddress
 ON vendor.VendorID = vendoraddress.VendorID
 
 LEFT JOIN `tc-da-1.adwentureworks_db.address` AS address 
