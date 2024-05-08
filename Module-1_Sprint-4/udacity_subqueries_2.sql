e48dc8e05242d34b6715be1fa996aaa4eaa9bce2
Giedrius77 <giedrius.zalionis@gmail.com>
2024-05-08 13:45:24 +0300

    udacity_subqueries_2 task completed

--- Module-1_Sprint-4/udacity_subqueries_2.sql
+++ Module-1_Sprint-4/udacity_subqueries_2.sql
@@ -1,26 +1,69 @@
 -- 2. For the region with the largest (sum) of sales total_amt_usd, 
 -- how many total (count) orders were placed?
 
-SELECT r.name AS region_name, 
-    SUM(o.total_amt_usd) AS max_total_amt_usd, 
-    COUNT(o.id) AS total_order
-FROM sales_reps AS sr
-JOIN region AS r
-ON sr.region_id = r.id
-JOIN accounts AS a
-ON a.sales_rep_id = sr.id
-JOIN orders AS o
-ON o.account_id = a.id
-GROUP BY region_name
-ORDER BY max_total_amt_usd DESC;
+/*1st STEP:
+The first query I wrote was to pull the total_amt_usd for each region.*/
+SELECT r.name AS region_name, SUM(o.total_amt_usd) AS total_amt
+    FROM orders AS o
+    JOIN accounts AS a
+    ON o.account_id = a.id
+    JOIN sales_reps AS sr
+    ON a.sales_rep_id = sr.id
+    JOIN region AS r
+    ON sr.region_id = r.id
+GROUP BY region_name;
 
--- Udacity solution
-SELECT r.name region_name, SUM(o.total_amt_usd) total_amt
-FROM sales_reps s
-JOIN accounts a
-ON a.sales_rep_id = s.id
-JOIN orders o
-ON o.account_id = a.id
-JOIN region r
-ON r.id = s.region_id
-GROUP BY r.name;
+/*2nd STEP:
+Then we just want the region with the max amount from this table. 
+There are two ways I considered getting this amount.
+One was to pull the max using a subquery. Another way is to order descending and just pull the top value.*/
+
+SELECT region_name, MAX(total_amt) AS total_amt
+    FROM
+        (SELECT r.name AS region_name, SUM(o.total_amt_usd) AS total_amt
+            FROM orders AS o
+            JOIN accounts AS a
+            ON o.account_id = a.id
+            JOIN sales_reps AS sr
+            ON a.sales_rep_id = sr.id
+            JOIN region AS r
+            ON sr.region_id = r.id
+        GROUP BY region_name) t1
+GROUP BY 1;
+
+/* OR , same result without subquery */
+
+SELECT r.name AS region_name, SUM(o.total_amt_usd) AS total_amt
+    FROM orders AS o
+    JOIN accounts AS a
+    ON o.account_id = a.id
+    JOIN sales_reps AS sr
+    ON a.sales_rep_id = sr.id
+    JOIN region AS r
+    ON sr.region_id = r.id
+GROUP BY region_name
+ORDER BY total_amt DESC;
+
+/*3rd STEP:
+Finally, we want to pull the total orders for the region with this amount: This provides the Northeast with 2357 orders.*/
+SELECT r.name AS region_name, COUNT(o.total) AS total_order
+FROM orders AS o
+    JOIN accounts AS a
+    ON o.account_id = a.id
+    JOIN sales_reps AS sr
+    ON a.sales_rep_id = sr.id
+    JOIN region AS r
+    ON sr.region_id = r.id
+GROUP BY 1
+HAVING SUM(o.total_amt_usd) = (
+    SELECT MAX(total_amt) AS total_amt
+    FROM
+        (SELECT r.name AS region_name, SUM(o.total_amt_usd) AS total_amt
+            FROM orders AS o
+            JOIN accounts AS a
+            ON o.account_id = a.id
+            JOIN sales_reps AS sr
+            ON a.sales_rep_id = sr.id
+            JOIN region AS r
+            ON sr.region_id = r.id
+        GROUP BY 1) t1);
