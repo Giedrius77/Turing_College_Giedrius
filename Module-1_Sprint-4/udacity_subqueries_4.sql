2e76e1411f137ff5332c0e22a374277195c2ec87
Giedrius77 <giedrius.zalionis@gmail.com>
2024-05-08 14:44:03 +0300

    udacity_subqueries_4 completed

--- Module-1_Sprint-4/udacity_subqueries_4.sql
+++ Module-1_Sprint-4/udacity_subqueries_4.sql
@@ -1,2 +1,31 @@
 -- 4. For the customer that spent the most (in total over their lifetime as a customer) 
 -- total_amt_usd, how many web_events did they have for each channel?
+
+/* 1st STEP:
+Here, we first want to pull the customer with the most spent in lifetime value.*/
+
+SELECT a.id, a.name, SUM(o.total_amt_usd) AS total_amt_usd
+FROM accounts AS a
+JOIN orders AS o
+ON o.account_id = a.id
+GROUP BY 1, 2
+ORDER BY 3 DESC
+LIMIT 1;
+
+/* 2nd STEP:
+Now, we want to look at the number of events on each channel this company had, 
+which we can match with just the id.*/
+
+SELECT a.name, we.channel, COUNT(*) AS total_event
+FROM accounts AS a
+JOIN web_events AS we
+ON we.account_id = a.id AND a.id = (SELECT id 
+                        FROM(SELECT a.id, a.name, SUM(o.total_amt_usd) AS total_amt_usd
+                                FROM accounts AS a
+                                JOIN orders AS o
+                                ON o.account_id = a.id
+                                GROUP BY 1, 2
+                                ORDER BY 3 DESC
+                                LIMIT 1) t1)
+GROUP BY 1, 2
+ORDER BY 3 DESC;
