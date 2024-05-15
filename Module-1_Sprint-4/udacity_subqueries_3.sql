813e3f2e191c9a7370156b52ab5d761203a60377
Giedrius77 <giedrius.zalionis@gmail.com>
2024-05-15 15:19:37 +0300

    subquery 3 using WITH

--- Module-1_Sprint-4/udacity_subqueries_3.sql
+++ Module-1_Sprint-4/udacity_subqueries_3.sql
@@ -48,3 +48,44 @@ FROM (SELECT a.name AS account_name
                                     GROUP BY 1
                                     ORDER BY 2 DESC
                                     LIMIT 1) t1)) t2;
+
+/* 4th STEP using WITH */
+
+WITH t1 AS (SELECT a.name AS account_name, 
+                SUM(o.standard_qty) AS total_standard_paper, 
+                SUM(o.total) AS total
+                        FROM orders AS o
+                        JOIN accounts AS a
+                        ON o.account_id = a.id
+                        GROUP BY 1
+                        ORDER BY 2 DESC
+                        LIMIT 1),
+t2 AS (SELECT a.name AS account_name
+FROM orders AS o
+JOIN accounts AS a
+ON o.account_id = a.id
+GROUP BY 1
+HAVING SUM(o.total) > (SELECT total FROM t1))
+
+SELECT COUNT(*)
+FROM t2;
+
+/* Udacity */
+
+WITH t1 AS (
+      SELECT a.name account_name, SUM(o.standard_qty) total_std, SUM(o.total) total
+      FROM accounts a
+      JOIN orders o
+      ON o.account_id = a.id
+      GROUP BY 1
+      ORDER BY 2 DESC
+      LIMIT 1), 
+t2 AS (
+      SELECT a.name
+      FROM orders o
+      JOIN accounts a
+      ON a.id = o.account_id
+      GROUP BY 1
+      HAVING SUM(o.total) > (SELECT total FROM t1))
+SELECT COUNT(*)
+FROM t2;
