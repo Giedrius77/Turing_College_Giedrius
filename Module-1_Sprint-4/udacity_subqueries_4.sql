61759442248ceea21439d66d3e9f35f1f2f08aa8
Giedrius77 <giedrius.zalionis@gmail.com>
2024-05-15 15:29:19 +0300

    subquery 4 using WITH

--- Module-1_Sprint-4/udacity_subqueries_4.sql
+++ Module-1_Sprint-4/udacity_subqueries_4.sql
@@ -29,3 +29,37 @@ ON we.account_id = a.id AND a.id = (SELECT id
                                 LIMIT 1) t1)
 GROUP BY 1, 2
 ORDER BY 3 DESC;
+
+/* 4th STEP using WITH */
+
+WITH t1 AS (SELECT a.id, a.name, SUM(o.total_amt_usd) AS total_amt_usd
+                FROM accounts AS a
+                JOIN orders AS o
+                ON o.account_id = a.id
+                GROUP BY 1, 2
+                ORDER BY 3 DESC
+                LIMIT 1)
+
+SELECT a.name, we.channel, COUNT(*) AS total_event
+FROM accounts AS a
+JOIN web_events AS we
+ON we.account_id = a.id AND a.id = (SELECT id FROM t1)
+GROUP BY 1, 2
+ORDER BY 3 DESC;
+
+/* Udacity */
+
+WITH t1 AS (
+      SELECT a.id, a.name, SUM(o.total_amt_usd) tot_spent
+      FROM orders o
+      JOIN accounts a
+      ON a.id = o.account_id
+      GROUP BY a.id, a.name
+      ORDER BY 3 DESC
+      LIMIT 1)
+SELECT a.name, w.channel, COUNT(*)
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id AND a.id =  (SELECT id FROM t1)
+GROUP BY 1, 2
+ORDER BY 3 DESC;
